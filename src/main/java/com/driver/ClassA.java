36df25ec4ccfc85dee930f76fe9f11dfe7255dfb
vallurukeerti <120296082+vallurukeerti@users.noreply.github.com>
2023-02-08 20:35:53 +0530

    First Assignment1

---
+++ src/main/java/com/driver/ClassA.java
@@ -0,0 +1,7 @@
+package com.driver;
+
+public class A {
+    String meth() {
+        System.out.print("Invoking method from class A");
+    }
+}
---
+++ src/main/java/com/driver/ClassB.java
@@ -0,0 +1,7 @@
+package com.driver;
+
+public class B extends A {
+    String meth(){
+        System.out.print("Method is overridden in Extendend class B");
+    }
+}
